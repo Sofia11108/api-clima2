98d2e1c65d1be8052ca0c986a474e144238cb3f4
sofii11108 <sofia35025@gmail.com>
2023-08-30 18:17:47 -0500

    cambios en la url que apunta al back desplegado

--- Frontend/src/app/components/login/login.component.ts
+++ Frontend/src/app/components/login/login.component.ts
@@ -33,7 +33,7 @@ export class LoginComponent implements OnInit {
   onSubmit() {
 
     if (this.form.valid) {
-      this.client.postRequest("http://localhost:10101/login",
+      this.client.postRequest("https://api-clima2.onrender.com/login",
         {
           email: this.form.value.email,
           password: this.form.value.password
